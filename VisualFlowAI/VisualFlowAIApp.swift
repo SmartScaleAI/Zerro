add3eb9ab44601f3d1aefb30ae53b5cabcffba74
Colin <colinmbreeding@gmail.com>
2026-05-27 11:52:47 -0400

    Initial Commit

---
+++ VisualFlowAI/VisualFlowAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualFlowAIApp.swift
+//  VisualFlowAI
+//
+//  Created by Colin Breeding on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct VisualFlowAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
